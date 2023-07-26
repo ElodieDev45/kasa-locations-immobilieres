7d8c30a5bbe373b39310b8854b662cc8c693f444
ElodieD45 <119341273+ElodieDev45@users.noreply.github.com>
2023-07-26 20:26:22 +0200

    import components dans Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,17 @@
 import './Home.css'
 import Header from '../../components/header/header'
+import Banner from '../../components/banner/banner'
+import Card from '../../components/card/card'
+import Footer from '../../components/footer/footer'
 
 function Home() {
     return(
         <div>
             Home
             <Header />
+            <Banner />
+            <Card />
+            <Footer />
         </div>
     )
 }
