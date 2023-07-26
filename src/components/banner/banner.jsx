76c44f2de67e5a76a282f2c0c293befec1c05338
ElodieD45 <119341273+ElodieDev45@users.noreply.github.com>
2023-07-26 23:10:12 +0200

    modif banner

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,8 +1,10 @@
+import banner from '../assets/banner.png'
 import './banner.css'
 
 function Banner() {
     return (
       <div className='Banner'>
+        <img src={banner} alt='paysage de la bannière' className='Banner-img' />
         <h2 className='Banner-title'>Banner</h2>
       </div>
     );
