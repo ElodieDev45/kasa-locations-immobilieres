db0b44e1e00f5d93bb4dc82beee1c69f46cb9e7c
ElodieD45 <119341273+ElodieDev45@users.noreply.github.com>
2023-07-29 00:03:39 +0200

    fichiers datas

---
+++ src/datas/logements2.js
@@ -0,0 +1,66 @@
+export const logements = 
+[
+	{
+		id: "c67ab8a7",
+		title: "Appartement cosy",
+		cover: "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
+		"pictures": [
+			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
+			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-2.jpg",
+			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-3.jpg",
+			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-4.jpg",
+			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-5.jpg"
+		],
+		description: "Votre maison loin de chez vous. Que vous veniez de l'autre bout du monde, ou juste de quelques stations de RER, vous vous sentirez chez vous dans notre appartement.",
+		host: {
+			name: "Nathalie Jean",
+			picture: "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-12.jpg"
+		},
+		rating: "5",
+		location: "Ile de France - Paris 17e",
+		equipments: [
+			"Équipements de base",
+			"Micro-Ondes",
+			"Douche italienne",
+			"Frigo",
+			"WIFI"
+		],
+		tags: [
+			"Batignolle",
+			"Montmartre"
+		]
+	},
+	{
+		id: "b9123946",
+		title: "Magnifique appartement proche Canal Saint Martin",
+		cover: "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-1.jpg",
+		pictures: [
+			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-1.jpg",
+			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-2.jpg",
+			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-3.jpg",
+			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-4.jpg",
+			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-5.jpg",
+			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-6.jpg"
+		],
+		description: "Profitez du charme de la vie parisienne dans un magnifique appartement. A 3 minutes à pied du Canl Saint Martin, vous serez proche des transports, mais également de nombreux commerces. L'appartement est tout équipé, et possède également un parking pour ceux qui souhaitent se déplacer en voiture.",
+		host: {
+			"name": "Della Case",
+			"picture": "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-1.jpg"
+		},
+		rating: "4",
+		location: "Ile de France - Paris 10e",
+		equipments: [
+			"Parking",
+			"Sèche Cheveux",
+			"Machine à laver",
+			"Wi-fi",
+			"Cuisine équipée",
+			"Télévision"
+		],
+		tags: [
+			"Canal Saint Martin",
+			"République",
+			"Appartement"
+		]
+	},
+]
