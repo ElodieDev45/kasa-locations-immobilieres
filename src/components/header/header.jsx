e5f42d35de8a403a366f5fead378083be4ceabda
ElodieD45 <119341273+ElodieDev45@users.noreply.github.com>
2023-07-28 14:00:56 +0200

    modifs Header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,16 @@
+import logo from '../assets/logo.png'
 import './header.css'
 
 function Header() {
     return (
       <div className='Header'>
-        <h1 className='Header-title'>Header</h1>
+        <img src={logo} alt='logo Kasa' className='Header-logo' />
+        <nav className='Header-nav'>
+          <ul>
+            <li><a href="../../">Accueil</a></li>
+            <li><a href='../../About'>A propos</a></li>
+          </ul>
+        </nav>
       </div>
     );
   }
