e3ac1e958f9d9d0555829960d600e95176ccb556
ElodieD45 <119341273+ElodieDev45@users.noreply.github.com>
2023-07-30 23:20:59 +0200

    comment app en attente

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,12 @@
-import '../styles/App.css';
-import Header from './Header';
+// import '../styles/App.css';
+// import Header from './Header';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
