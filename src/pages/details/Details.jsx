00ae55d11a567c90c460524a831730c76d6bd486
ElodieD45 <119341273+ElodieDev45@users.noreply.github.com>
2023-08-01 20:17:45 +0200

    Import données fiches logement détails

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -2,14 +2,32 @@ import './Details.css'
 import Header from '../../components/header/header'
 import FicheLogement from '../../components/ficheLogement/ficheLogement'
 import Footer from '../../components/footer/footer'
+import datasLogements from '../../datas/logements.json'
+
 
 function Details() {
+    const idLogement =  new URLSearchParams(window.location.search).get('id');
+    console.log('id logement :', idLogement);
+    
+    const logementInfos = datasLogements.find((logement) => logement.id === idLogement)
+    console.log('infos logement :', logementInfos);
+    
     return(
         <div id="Details">
-            Details
             <Header />
             <main>
-                <FicheLogement />
+                <FicheLogement 
+                    id={logementInfos.id}
+                    title={logementInfos.title}
+                    pictures={logementInfos.pictures}
+                    description={logementInfos.description}
+                    hostName={logementInfos.hostName}
+                    hostPicture={logementInfos.hostPicture}
+                    rating={logementInfos.rating}
+                    location={logementInfos.location}
+                    equipments={logementInfos.equipments}
+                    tags={logementInfos.tags}
+                />
             </main>
             <Footer />
         </div>
