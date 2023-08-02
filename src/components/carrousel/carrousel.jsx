86a5e355f3deb2bdf8188318d1d45db99141ba2f
ElodieD45 <119341273+ElodieDev45@users.noreply.github.com>
2023-08-02 06:08:54 +0200

    creation Carrousel

--- src/components/carrousel/carrousel.jsx
+++ src/components/carrousel/carrousel.jsx
@@ -1,14 +1,32 @@
+import '../carrousel/carrousel.css'
+import { useState } from "react"
+import previous from '../../assets/previous.png'
+import next from  '../../assets/next.png'
+
 function Carrousel({datasid, title, pictures}) {
+    const [index, setIndex] = useState(0);
+    const length = pictures.length;
+
+    const handlePrevious = () => {
+        const newIndex = index - 1;
+        setIndex(newIndex < 0 ? length - 1 : newIndex);
+    };
+
+    const handleNext = () => {
+        const newIndex = index + 1;
+        setIndex(newIndex >= length ? 0 : newIndex);
+    };
+
     return (
         <div className="carroussel" key={datasid}>
             <figure>
-                {pictures.map((picture, index) =>(
+                <button onClick={handlePrevious} id='previous' ><img src={previous} className='scrollPrevious' alt='flèche précédent'/></button>
                     <img
-                        key={index} 
-                        src={picture}
+                        src={pictures[index]}
                         alt={`aperçu du logement ${title}`}
+                        className='pctLogement'
                     />
-                ))}
+                <button onClick={handleNext} id='next'><img src={next} className='scrollNext' alt='flèche suivant'/></button>
             </figure>
         </div>
     )
