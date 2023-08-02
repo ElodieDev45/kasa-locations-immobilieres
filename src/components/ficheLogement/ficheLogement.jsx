da35063dcb927078c56104acf655dea941b81c3f
ElodieD45 <119341273+ElodieDev45@users.noreply.github.com>
2023-08-02 06:09:37 +0200

    ajustements ficheLogement

--- src/components/ficheLogement/ficheLogement.jsx
+++ src/components/ficheLogement/ficheLogement.jsx
@@ -1,3 +1,5 @@
+import '../ficheLogement/ficheLogement.css'
+
 import Carrousel from "../carrousel/carrousel"
 import Dropdown from "../dropdown/dropdown"
 import Tags from "../tags/tags"
@@ -7,46 +9,48 @@ import Rate from "../rate/rate"
 
 function FicheLogement({datasid, title, pictures, description, hostName, hostPicture, rating, location, equipments, tags}) {
     return (
-        <main>
-            <div>
-                <Carrousel
-                    datasid={datasid} 
-                    title={title}
-                    pictures={pictures}
-                />
-                <section>
+        <section className="ficheLogement">
+            <Carrousel
+                datasid={datasid} 
+                title={title}
+                pictures={pictures}
+            />
+            <div className='ficheLogement-nh'>
+                <div className="ficheLogement-name">
                     <h2 datasid={datasid} >{title}</h2>
                     <p datasid={datasid} >{location}</p>
-                </section>
-                <aside>
+                </div>
+                <aside className="ficheLogement-host">
                     <Host
                         datasid={datasid}
                         name={hostName}
                         picture={hostPicture}
                     />
                 </aside>
-                <Rate
-                    datasid={datasid} 
-                    rating={rating}
-                />
+            </div>
+            <div className='ficheLogement-tr'>
                 <Tags
                     datasid={datasid} 
                     tags={tags}            
                 />
-                <div>
-                    <Dropdown
-                        datasid={datasid} 
-                        title='Description'
-                        content={description}
-                    />
-                    <Dropdown
-                        datasid={datasid}
-                        title='Equipements'
-                        content={equipments}
-                    />
-                </div>
+                <Rate
+                    datasid={datasid} 
+                    rating={rating}
+                />
             </div>
-        </main>
+            <div className="ficheLogement-details">
+                <Dropdown
+                    datasid={datasid} 
+                    title='Description'
+                    content={description}
+                />
+                <Dropdown
+                    datasid={datasid}
+                    title='Equipements'
+                    content={equipments}
+                />
+            </div>
+        </section>
     )
 }
 
