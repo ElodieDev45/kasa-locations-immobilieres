706ddb48d3970c04e77dec0107738a3c55ed1fbe
ElodieD45 <119341273+ElodieDev45@users.noreply.github.com>
2023-08-02 22:36:05 +0200

    ajustement composant page erreur

--- src/components/errorComponent/errorComponent.jsx
+++ src/components/errorComponent/errorComponent.jsx
@@ -5,7 +5,7 @@ function ErrorComponent({errorImg, errorText, altImg}) {
         <div className='error-component'>
             <div className="error-body">
                 <img src={errorImg} alt={altImg} className='error-img' />
-                <h2>{errorText}</h2>
+                <h1>{errorText}</h1>
             </div>
             <a href="../../">Retourner sur la page d'acceuil</a>
         </div>
