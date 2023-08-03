8eb7fac898da64daa2ac188463cef1c33421625c
ElodieD45 <119341273+ElodieDev45@users.noreply.github.com>
2023-08-03 23:57:19 +0200

    créaa json About et dropdowns

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,6 +3,8 @@ import Header from '../../components/header/header'
 import Footer from '../../components/footer/footer'
 import Banner from '../../components/banner/banner'
 import imgAbout from '../../assets/banner-about.png'
+import aboutDatas from '../../datas/about.json'
+import Dropdown from '../../components/dropdown/dropdown'
 
 
 function About() {
@@ -17,6 +19,44 @@ function About() {
                     alt={'paysage de montagne et rivière'}
                 />
             </section>
+            <main className='Dropdown'>
+                <article className='About-dropdown'>
+                    {aboutDatas.map(({id, Reliability}) => (
+                        <Dropdown 
+                            id={id}
+                            title="Fiabilité"
+                            content={Reliability}
+                        />
+                    ))}
+                </article>
+                <article className='About-dropdown'>
+                    {aboutDatas.map(({id, Respect}) => (
+                        <Dropdown 
+                            id={id}
+                            title="Respect"
+                            content={Respect}
+                        />
+                    ))}
+                </article>
+                <article className='About-dropdown'>
+                    {aboutDatas.map(({id, Service}) => (
+                        <Dropdown 
+                            id={id}
+                            title="Service"
+                            content={Service}
+                        />
+                    ))}
+                </article>
+                <article className='About-dropdown'>
+                    {aboutDatas.map(({id, Security}) => (
+                        <Dropdown 
+                            id={id}
+                            title="Sécurité"
+                            content={Security}
+                        />
+                    ))}
+                </article>
+            </main>
             <footer>
                 <Footer />
             </footer>
