73b538225a947621d5424eb1a74e51e525de70c7
ElodieD45 <119341273+ElodieDev45@users.noreply.github.com>
2023-08-06 15:34:14 +0200

    modif nav en nalink

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,17 +1,32 @@
 import logo from '../../assets/logo.png'
+import { NavLink } from 'react-router-dom';
 
 function Header() {
     return (
-      <div className='Header'>
-        <img src={logo} alt='logo Kasa' className='Header-logo' />
-        <nav className='Header-nav'>
-          <ul>
-            <li><a href="../../">Accueil</a></li>
-            <li><a href='../../About'>A propos</a></li>
-          </ul>
-        </nav>
-      </div>
+        <div className='Header'>
+            <img src={logo} alt='logo Kasa' className='Header-logo' />
+            <nav className='Header-nav'>
+                <ul>
+                    <li>
+                        <NavLink 
+                          to="../../" className={({ isActive, isPending }) =>
+                            isPending ? "pending" : isActive ? "active" : ""}
+                        >
+                            Accueil
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='../../About' className={({ isActive, isPending }) =>
+                            isPending ? "pending" : isActive ? "active" : ""}
+                        >
+                          A propos
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     );
-  }
+}
   
 export default Header;
