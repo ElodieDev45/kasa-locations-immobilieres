16c4e9cb1ff8fcee486fb41eb51bba4d746f5bd1
ElodieD45 <119341273+ElodieDev45@users.noreply.github.com>
2023-08-11 13:05:14 +0200

    seeparation Index et App

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Home from './pages/home/Home';
+import Details from './pages/details/Details';
+import About from './pages/about/About';
+import Error404 from './pages/404/404';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import '../src/styles/styles.scss';
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/home' element={<Home />} />
+                <Route path='/details/:id' element={<Details />} />
+                <Route path='/about' element={<About />} />
+                <Route path='*' element={<Error404 />} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
 import React from 'react';
-import Home from './pages/home/Home';
-import Details from './pages/details/Details';
-import About from './pages/about/About';
-import Error404 from './pages/404/404';
 import {createRoot} from 'react-dom/client';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import App from './App.js'
 import '../src/styles/styles.scss';
 
-const App = () => (
-  <>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/home' element={<Home />} />
-      <Route path='/details/:id' element={<Details />} />
-      <Route path='/about' element={<About />} />
-      <Route path='*' element={<Error404 />} />
-    </Routes>
-  </>
-)
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
       <App />
-    </Router>
   </React.StrictMode>
 );
-
-// reportWebVitals();
