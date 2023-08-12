40514f6e625655744cb015e5e3261ab55cb91de9
ElodieD45 <119341273+ElodieDev45@users.noreply.github.com>
2023-08-12 20:09:37 +0200

    ajout comments & modif dropdown dynamique/sections

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,21 +1,29 @@
+//import fonctions spéciales React (hook)
+import { useParams, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+//import fichier de données au format JSON
+import datasLogements from '../../datas/logements.json'
+//import composants
 import Header from '../../components/header/header'
 import FicheLogement from '../../components/ficheLogement/ficheLogement'
 import Footer from '../../components/footer/footer'
-import datasLogements from '../../datas/logements.json'
-import { useParams, useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
 
 function Details() {
+    //const pour permettre la navigation vers une autre page
     const navigate = useNavigate();
+    //const pour récupérer l'id
     const {id} =  useParams();
+    //const qui renvoi les données du logement ou un statut undefined
     const logementInfos = datasLogements.find((logement) => logement.id === id);
 
+    //gestion des pages inexistantes
+    //conditions de renvoi vers page d'erreur si logementInfos est en statut "Undefined"
     useEffect(() =>{
         if (!logementInfos) {
             navigate('*');
         }
     }, [logementInfos, navigate]);
-
+    //conditions empêchant le rendu du reste de la page si logementInfos est statut "Undefined"
     if (!logementInfos) {
         return null;
     }
@@ -23,23 +31,29 @@ function Details() {
     return(
         <div id="Details">
             <header>
+                {/* intégration composant de l'en-tête */}
                 <Header />
             </header>
             <main>
+                {/* intégration composant des détails du logement */}
                 <FicheLogement 
                     datasid={logementInfos.id}
                     title={logementInfos.title}
                     pictures={logementInfos.pictures}
-                    description={logementInfos.description}
                     hostName={logementInfos.host.name}
                     hostPicture={logementInfos.host.picture}
                     rating={logementInfos.rating}
                     location={logementInfos.location}
-                    equipments={logementInfos.equipments}
                     tags={logementInfos.tags}
+                    //ajout d'un tableau "sections" évolutif pour catégories des dropdown
+                    sections={[
+                        {title: 'Description', content: logementInfos.description},
+                        {title: 'Équipements', content: logementInfos.equipments.join(', ')}
+                    ]}
                 />
             </main>
             <footer>
+                {/* intégration composant de pied de page */}
                 <Footer />
             </footer>
         </div>
