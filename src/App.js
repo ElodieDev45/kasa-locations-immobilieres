14b23fc1139700ed934cd0bbd3c29bb152e46f72
ElodieD45 <119341273+ElodieDev45@users.noreply.github.com>
2023-08-12 22:37:08 +0200

    ajouts comments fichiers js base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+//import pages
 import Home from './pages/home/Home';
 import Details from './pages/details/Details';
 import About from './pages/about/About';
 import Error404 from './pages/404/404';
+//creation router
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+//import fichier styles
 import '../src/styles/styles.scss';
 
 //creation des routes de l'application
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+//import fonctions spéciales React (hook)
 import {createRoot} from 'react-dom/client';
+//import lien vers App
 import App from './App.js'
+//import fichier styles
 import '../src/styles/styles.scss';
 
+//création de la racine de l'application dans le DOM
 const root = createRoot(document.getElementById('root'));
+
+//rendu du composant App dans le strictMode react pour affichage des erreurs ou conflits possibles
 root.render(
   <React.StrictMode>
       <App />
